32d14b7ad91c329b486264435b66a384df8efccf
Veronika L <verokipri@yahoo.com>
2021-09-11 11:32:53 +0200

    week14

---
+++ week14-JS/index.js
@@ -0,0 +1,54 @@
+
+
+
+function add(){
+    let a = document.querySelector(`#number1`).value;
+    let b = document.querySelector(`#number2`).value;
+    let c = +a + +b;
+    console.log(c);
+
+    document.querySelector(`#result1`).value = c;   
+}
+
+function minus(){
+    let a = document.querySelector(`#number3`).value;
+    let b = document.querySelector(`#number4`).value;
+    let c = a - b;
+    console.log(c);
+
+    document.querySelector(`#result2`).value = c;   
+}
+
+function multiply(){
+    let a = document.querySelector(`#number5`).value;
+    let b = document.querySelector(`#number6`).value;
+    let c = a * b;
+    console.log(c);
+
+    document.querySelector(`#result3`).value = c;   
+}
+
+function divide(){
+    let a = document.querySelector(`#number7`).value;
+    let b = document.querySelector(`#number8`).value;
+    let c = a / b;
+    console.log(c);
+
+    document.querySelector(`#result4`).value = c;   
+}
+
+/*Task 2*/
+
+function magic(sender){
+    sender.classList.add(`selected`);
+}
+
+/*Task 3*/
+
+const back = document.querySelector(`#back`);
+const next = document.querySelector(`#next`);
+
+
+
+
+
