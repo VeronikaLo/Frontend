39d4b6e774e95af0946e38d20c2f7d20e31ee589
Veronika L <verokipri@yahoo.com>
2021-09-28 09:51:28 +0200

    Task2

--- week16-JS/index.js
+++ week16-JS/index.js
@@ -37,101 +37,132 @@ sumInput();
 //Task 2
 
 //create neu options
-let marke = document.querySelector(`#marke`);
-marke.addEventListener(`change`, function(){
 
-let markeValue = document.querySelector(`#marke`).value;
-  
-let audis =['A5','А6','TT',`SQ 7`,`SQ 8`,`V8`];
-let mercedeses =[`A-Klasse`,`B-Klasse`,`C-Klasse`,`E-Klasse`,`G-Klasse`,`CLK`];
-let volkswagens =[`Golf`,`Polo`,`Sharan`,`Touran`,`Passat`,`Käfer`,`Pointer`];
-let toyotas =[`Auris`,`Avensis`,`Camry`,`Highlander`,`RAV 4`,`Verso`,`Yaris`];
-let bmws =[`5-er`,`6-er`,`7-er`,`8-er`,`M-Reihe`,`X-Reihe`,`Z-Reihe`];
+
+let marke = document.querySelector(`#marke`);
+
+marke.addEventListener(`change`, function () {
+
+  let markeValue = document.querySelector(`#marke`).value;
+  console.log(markeValue);
+
+  let audis = [
+    'A5', 1000,
+    'А6', 2000,
+    'TT', 3000,
+    `SQ 7`, 4000,
+    `SQ 8`, 5000
+  ];
+
+  let mercedeses = [
+    `A-Klasse`, 3500,
+    `B-Klasse`, 4500,
+    `C-Klasse`, 5500,
+    `E-Klasse`, 5000,
+    `CLK`, 6500
+  ];
+
+  let volkswagens = [
+    `Golf`, 700,
+    `Polo`, 600,
+    `Sharan`, 800,
+    `Touran`, 1000,
+    `Passat`, 9000
+  ];
+
+  let toyotas = [
+    `Auris`, 700,
+    `Avensis`, 2000,
+    `Camry`, 2500,
+    `Highlander`, 3500,
+    `RAV 4`, 3000
+  ];
+
+  let bmws = [
+    `7-er`, 2000,
+    `8-er`, 2800,
+    `M-Reihe`, 4000,
+    `X-Reihe`, 3500,
+    `Z-Reihe`, 3000
+  ];
 
 
   let modellValue = [];
   let modell = document.querySelector(`#modell`);
-  document.querySelector(`#modell`).innerHTML ="";
-  
+  document.querySelector(`#modell`).innerHTML = "";
 
-  if(markeValue==0){
+
+  if (markeValue == 0) {
     return
-  }
-  else if(markeValue==13000){
+  } else if (markeValue == 13000) {
     modellValue = [...audis]
-  }
-  else if(markeValue==17000){
+  } else if (markeValue == 17000) {
     modellValue = [...mercedeses]
-  }
-  else if(markeValue==10000){
+  } else if (markeValue == 10000) {
     modellValue = [...volkswagens]
-  }
-  else if(markeValue==11000){
+  } else if (markeValue == 11000) {
     modellValue = [...toyotas]
-  }
-  else if(markeValue==15000){
+  } else if (markeValue == 15000) {
     modellValue = [...bmws]
   }
- 
-  for( val of modellValue){
-        let option = document.createElement("option");
-        option.value = val;
-        option.text = val.charAt(0).toUpperCase() + val.slice(1);
-        modell.append(option);
+
+
+  for (let i = 0; i < modellValue.length; i += 2) {
+    let option = document.createElement("option");
+    let val = modellValue[i];
+    let price = modellValue[i + 1];
+    option.value = price;
+    option.text = val.charAt(0).toUpperCase() + val.slice(1);
+    modell.append(option);
   }
+
 
 })
 
 // button 
 
 const button = document.querySelector(`#button`);
-button.addEventListener (`click`, getsum);
+button.addEventListener(`click`, getsum);
 
-const karosserie = document.querySelectorAll(`option[name="karosserie"]`);
-console.log(karosserie);
 const radioZulass = document.querySelectorAll(`input[name="zulassung"]`);
-console.log(radioZulass)
 const radioKraft = document.querySelectorAll(`input[name="kraft"]`);
 const radioGetriebe = document.querySelectorAll(`input[name="getriebe"]`);
 let total = document.querySelector(`#total`);
 
 
 
-function getsum (){
-    
-  let totalPrice= ``;
+function getsum() {
 
-  
-  for(drop of marke){
-    if(drop.selected){
-      totalPrice += parseInt(drop.value);
-    }
-  } 
-  
-  for(drop of karosserie){
-    if(drop.selected){
-      totalPrice += parseInt(drop.value);
-    }
-  } 
+  let totalPrice = 0;
 
-  for(radio of radioZulass){
-    if(radio.checked){
+  let karosserie = document.querySelector(`#karosserie`);
+
+  let karosserieValue = karosserie.options[karosserie.selectedIndex].value;
+
+  markeValue = document.querySelector(`#marke`).value;
+
+  price = document.querySelector(`#modell`).value;
+
+  for (radio of radioZulass) {
+    if (radio.checked) {
       totalPrice += parseInt(radio.value);
+      console.log(totalPrice);
     }
-  } 
+  }
 
-  for(radio of radioKraft){
-    if(radio.checked){
+  for (radio of radioKraft) {
+    if (radio.checked) {
+      totalPrice += parseInt(radio.value);
+      console.log(totalPrice)
+    }
+  }
+
+  for (radio of radioGetriebe) {
+    if (radio.checked) {
       totalPrice += parseInt(radio.value);
     }
   }
 
-  for(radio of radioGetriebe){
-    if(radio.checked){
-      totalPrice += parseInt(radio.value);
-    }
-  }
-
-total.value = totalPrice;
+  total.value = +karosserieValue + +markeValue + +price + +totalPrice;
 
 }
