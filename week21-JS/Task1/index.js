d25b6072b0a631b2e67d317728de53b10126b377
Veronika L <verokipri@yahoo.com>
2021-11-09 10:16:19 +0100

    Task1 added loop

--- week21-JS/Task1/index.js
+++ week21-JS/Task1/index.js
@@ -10,14 +10,15 @@ input.addEventListener("keydown", enter); // Прослушка на input
 
 //Extra function, чтобы search начался только после Enter 
  function enter(e){
-    if(e.keyCode ==13){
+    if(e.code === "Enter"){
         console.log(input.value);
         search(input.value);
     }
+    
  }
 
 
-//search
+//search process
  async function search(data){
 
     try{
@@ -25,49 +26,66 @@ input.addEventListener("keydown", enter); // Прослушка на input
     console.log(res);
     const result = await res.json();
     console.log(result);
-
-    let box = document.querySelector(`.output`);
-    let img1 = document.createElement(`img`);
-    img1.src = result.data[0].url;
-    img1.classList.add(`pics`);
-    console.log(img1.src);
-    box.appendChild(img1);
-
-    let img2 = document.createElement(`img`);
-    img2.src = `${result.data[1].url}`;
-    img2.classList.add(`pics`);
-    console.log(img2.src);
-    box.appendChild(img2);
-
-    let img3 = document.createElement(`img`);
-    img3.src = result.data[2].url;
-    img3.classList.add(`pics`);
-    console.log(img3.src);
-    box.appendChild(img3);
-
-    let img4 = document.createElement(`img`);
-    img4.src = result.data[3].url;
-    img4.classList.add(`pics`);
-    console.log(img4.src);
-    box.appendChild(img4);
-
-    let img5 = document.createElement(`img`);
-    img5.src = result.data[4].url;
-    img5.classList.add(`pics`);
-    console.log(img5.src);
-    box.appendChild(img5);
-
+    render(result);
 }
     catch(error){
     console.log(error);
 }
-    input.value ='';
-
+input.value ='';
     
-
-
-
 }
 
+function render(result){
+    let box = document.querySelector(`.output`);
+    for (let i =0; i< result.data.length; i++){
+        let img = document.createElement(`img`);
+        img.src = result.data[i].images.original.url;
+        img.classList.add(`pics`);
+        box.appendChild(img);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+//Function render() without a loop
+
+function render(result){
+    let box = document.querySelector(`.output`);
+    let img1 = document.createElement(`img`);
+    img1.src = result.data[0].images.original.url;
+    img1.classList.add(`pics`);
+    box.appendChild(img1);
+
+    let img2 = document.createElement(`img`);
+    img2.src = result.data[1].images.original.url;
+    img2.classList.add(`pics`);
+    box.appendChild(img2);
+
+    let img3 = document.createElement(`img`);
+    img3.src = result.data[2].images.original.url;
+    img3.classList.add(`pics`);
+    box.appendChild(img3);
+
+    let img4 = document.createElement(`img`);
+    img4.src = result.data[3].images.original.url;
+    img4.classList.add(`pics`);
+    box.appendChild(img4);
+
+    let img5 = document.createElement(`img`);
+    img5.src = result.data[4].images.original.url;
+    img5.classList.add(`pics`);
+    box.appendChild(img5);   
+}
+*/
     
 
