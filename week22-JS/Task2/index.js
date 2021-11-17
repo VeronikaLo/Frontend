d049b6365763812366771c988acff10caca8a56a
Veronika L <verokipri@yahoo.com>
2021-11-17 16:13:14 +0100

    week22-2

--- week22-JS/Task2/index.js
+++ week22-JS/Task2/index.js
@@ -1,3 +1,17 @@
+//anime
+
+const anime = require(`animejs`);
+
+anime({
+    targets:'.chart',
+    duration: 5000,
+    backgroundColor:'#ffea00'
+});
+
+
+
+
+//moment
 const moment = require ('moment');
 
 let now = moment();
@@ -8,33 +22,45 @@ console.log (date);
 document.querySelector(`#time`).innerHTML = date;
 
 
+// chart
+let dataChart = {
+    weekDays:["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+    total:[10,9,10,11,9,4,6],
+    work:[9,9,6,9,5,0,0],
+    course:[1,0,3,2,4,4,5],
+    sport:[0,0,1,0,0,0,1]
+
+}
+
+
+
 
 const Chart = require('chart.js');
 const ctx = document.getElementById('myChart').getContext('2d');
 const  myChart = new Chart(ctx, {
     type: 'line',
     data: {
-      labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+      labels: dataChart.weekDays ,
       datasets: [{ 
-          data: [10,10,9,9,9,3,4],
+          data: dataChart.total,
           label: "Total",
           borderColor: "#3e95cd",
           backgroundColor: "#7bb6dd",
           fill: false,
         }, { 
-          data: [9,9,6,9,5,0,0],
-          label: "Working hours",
+          data: dataChart.work,
+          label: "Dental office",
           borderColor: "#3cba9f",
           backgroundColor: "#71d1bd",
           fill: false,
         }, { 
-          data: [1,0,3,0,3,3,4],
-          label: "Coding activity",
+          data: dataChart.course,
+          label: "IT-Girls",
           borderColor: "#ffa500",
           backgroundColor:"#ffc04d",
           fill: false,
         }, { 
-          data: [0,1,0,0,1,0,0],
+          data: dataChart.sport,
           label: "Sport",
           borderColor: "#c45850",
           backgroundColor:"#d78f89",
