299a896cb7e282bf2744e45b2ce5f1076c249af0
Veronika L <verokipri@yahoo.com>
2021-12-03 00:33:50 +0100

    assets

--- Project_test/index.js
+++ Project_test/index.js
@@ -1,7 +1,9 @@
 let text = "Ты действительно готов увидеть нечто невероятно ужасное ? Не боишься ? Не говори, что я тебя не предупреждал...";
-
+const audio = document.querySelector(`#player`);
+audio.play();
 let i=0;
-speed =120;
+speed =180;
+
 
 function beginning(){
 
@@ -11,19 +13,27 @@ function beginning(){
        i++;
        setTimeout(beginning, speed);   
    }
+   
 }
 
-
+beginning();
 
   function weiter (){
-    
+
     let pic = document.createElement(`img`);
     pic.src = "assets/header_harry-potter.png";
     pic.classList.add("pic");
     document.querySelector(`.header`).appendChild(pic);
+    setTimeout(buttonDelay, 25000  );
 
-     beginning();
+     
 }
 
-   weiter();
+weiter();
+
+function buttonDelay(){
+    document.querySelector(`.btn`).style.display ="block";
+}
+
+  
 
